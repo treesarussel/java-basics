42dabbc82af83bfe1e61bf0ab031541bec7df624
treesarussel <treesarussel@gmail.com>
2024-07-10 19:46:49 +0530

    Assignment call a static method

---
+++ Call_A_Static_Method.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Call_A_Static_Method {//Program to call static Method
+	static void Staticmethod()
+	{
+		int a=10;
+		int b=13;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+	
+
+	public static void main(String[] args) 
+	{
+		Staticmethod();
+	}
+
+}
